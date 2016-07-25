66e835962dd09d208b7c93a65187ac8e50f19ab3
Hao Fu <fuhao8235@qq.com>
2016-07-25 10:35:14 +0800

    change graph to dir_graph

--- FIN.py
+++ FIN.py
@@ -3,30 +3,32 @@
 ###the important node in this network
 from numpy import *
 
-###This function can convert the file to a network graph
+###This function can convert the file to a network graph, this can return two
+###graphs, the first is an directed graph, which is the real network, and the second
+###is an undirected graph, which would be easy to find the connected subsets
 ###file2graph(filename,date)
-###return graph
+###return dir_graph,undir_graph
 
 def file2graph(filename,date):
     fr=open(filename)
     arrayOLines=fr.readlines()
     numberOfLines=len(arrayOLines)
 
-    graph={}
+    dir_graph={}
     
     for line in arrayOLines:
         line=line.strip()
         listFromLine=line.split('","')
         if(len(listFromLine)>5 and listFromLine[1]==date):
-            if(graph.has_key(listFromLine[2])==False):
-                graph[listFromLine[2]]={}
+            if(dir_graph.has_key(listFromLine[2])==False):
+                dir_graph[listFromLine[2]]={}
             else:
-                if(graph[listFromLine[2]].has_key(listFromLine[5])==False):
-                    graph[listFromLine[2]][listFromLine[5]]=1
-            if(graph.has_key(listFromLine[5])==False):
-                graph[listFromLine[5]]={}
+                if(dir_graph[listFromLine[2]].has_key(listFromLine[5])==False):
+                    dir_graph[listFromLine[2]][listFromLine[5]]=1
+            if(dir_graph.has_key(listFromLine[5])==False):
+                dir_graph[listFromLine[5]]={}
 
-    return graph
+    return dir_graph
 
 ###This function can calculate the company number for each month from 2003-01 to 2015-12
 ###ComNum(filename)
